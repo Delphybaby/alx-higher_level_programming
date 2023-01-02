599aeb51eb2516cafd39e306f19e673d3cbfc1d9
Delphybaby <me.ejiofor@unizik.edu.ng>
2023-01-02 08:23:41 +0100

    get

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+class Rectangle:
+        def __init__(self, length, width):
+            self.l = length
+            self.w = width
+        def define_rect(self):
+            print(int(f'{self.l}*{self.w}'))
