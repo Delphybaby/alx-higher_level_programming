216534a31f1d88b877b1a4eae9ae1a72db218c8c
Delphybaby <me.ejiofor@unizik.edu.ng>
2023-01-09 13:27:32 +0100

    0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+# 0-lookup.py
+"""Defines an object attribute lookup function."""
+
+
 def lookup(obj):
-    result = dir(Object)
+    """Return a list of an object's available attributes."""
+    result = dir(obj)
     print(result)
