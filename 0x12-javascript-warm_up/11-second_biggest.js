aaacca77552867e20aa156fefa91d905b140859c
Delphybaby <me.ejiofor@unizik.edu.ng>
2023-03-13 11:45:23 +0100

    js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
-let land = (1,2,3,6)
-	console.log(land)
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
+}
